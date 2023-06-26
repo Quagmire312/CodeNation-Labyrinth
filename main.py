972186716afdaf0c806250815331e7ce5e57850c
Quagmire312 <120139093+Quagmire312@users.noreply.github.com>
2023-06-26 14:53:13 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -15,7 +15,28 @@ Rooms = [
     [[],[],[],[],[]]
 ]
 
-CurrentCoords = []
+"""
+CurrentRoom = [x,y]
+Rooms[x,y]
+options = [N,S,E,W]
+
+if x == 0:
+    remove option for west
+elif x == 4:
+    remove option for east
+if y == 0:
+    remove option for north
+elif y == 4:
+    remove option for south
+
+
+for dir in options:
+
+"""
+
+
+
+
 
 Keys = 0
 #if keys = 5:
@@ -47,11 +68,64 @@ MonsterStats = [40,5,15]
 
 
 def SpawnMonsters():
+    global Monster1, Monster2, Monster3, Monster4, Monster5
+    Occupied = True
+
     Monster1 = []
+
+    while Occupied:
+        x = randint(0,4)
+        y = randint(0,4)
+        if Rooms[x][y] == [] and [x,y] != [2,2]:
+            Monster1 = [x,y]
+            Rooms[x][y] = "Monster 1"
+            break
+
+
     Monster2 = []
+
+    while Occupied:
+        x = randint(0,4)
+        y = randint(0,4)
+        if Rooms[x][y] == [] and [x,y] != [2,2]:
+            Monster1 = [x,y]
+            Rooms[x][y] = "Monster 2"
+            break
+
+
     Monster3 = []
+
+    while Occupied:
+        x = randint(0,4)
+        y = randint(0,4)
+        if Rooms[x][y] == [] and [x,y] != [2,2]:
+            Monster1 = [x,y]
+            Rooms[x][y] = "Monster 3"
+            break
+
+
     Monster4 = []
+
+    while Occupied:
+        x = randint(0,4)
+        y = randint(0,4)
+        if Rooms[x][y] == [] and [x,y] != [2,2]:
+            Monster1 = [x,y]
+            Rooms[x][y] = "Monster 4"
+            break
+
+
     Monster5 = []
+
+    while Occupied:
+        x = randint(0,4)
+        y = randint(0,4)
+        if Rooms[x][y] == [] and [x,y] != [2,2]:
+            Monster1 = [x,y]
+            Rooms[x][y] = "Monster 5"
+            break
+
+
     #Boss = []
 
 DropChance = 0.5
@@ -60,17 +134,72 @@ HealthChance = 0.8 # otherwise strength
 #spawn boss once all enemies are defeated
 
 def SpawnEvents():
+    global Event1, Event2, Event3
+    Occupied = True
+
     Event1 = []
+
+    while Occupied:
+        x = randint(0,4)
+        y = randint(0,4)
+        if Rooms[x][y] == [] and [x,y] != [2,2]:
+            Monster1 = [x,y]
+            Rooms[x][y] = "Event 1"
+            break
+
+
     Event2 = []
+
+    while Occupied:
+        x = randint(0,4)
+        y = randint(0,4)
+        if Rooms[x][y] == [] and [x,y] != [2,2]:
+            Monster1 = [x,y]
+            Rooms[x][y] = "Event 2"
+            break
+
+
     Event3 = []
+
+    while Occupied:
+        x = randint(0,4)
+        y = randint(0,4)
+        if Rooms[x][y] == [] and [x,y] != [2,2]:
+            Monster1 = [x,y]
+            Rooms[x][y] = "Event 3"
+            break
+
+
+
 
 #print(Rooms[0][4])
 
 def SpawnItems(): # 5 items - potions
-    pass
+    Occupied = True
+    for i in range(randint(3,7)):
+
+        RNG = randint(1,100)
+
+        if RNG/100 <= HealthChance:
+            while Occupied:
+                x = randint(0,4)
+                y = randint(0,4)
+                if Rooms[x][y] == [] and [x,y] != [2,2]:
+                    Monster1 = [x,y]
+                    Rooms[x][y] = "Health Potion"
+                    break
+        else:
+            while Occupied:
+                x = randint(0,4)
+                y = randint(0,4)
+                if Rooms[x][y] == [] and [x,y] != [2,2]:
+                    Monster1 = [x,y]
+                    Rooms[x][y] = "Strength Potion"
+                    break
 
 
 def SpawnBoss():
+    global Boss
     Boss = []
 
 
@@ -80,6 +209,81 @@ EmptyRoomDescriptions = ["There is nothing in this room", "You look closely at t
 def Lore():
     print("You awaken in a dark room, you see a tile ahead of you....")
 
-def GameLoop():
-    if Keys == 5:
-        SpawnBoss()
+def Move(direction, x,y):
+    NewX = x
+    NewY = y
+    Stuck = False
+    if direction == "North":
+        NewY-=1
+    elif direction == "South":
+        NewY+=1
+    elif direction == "West":
+        NewX-=1
+    else:
+        NewX+=1
+
+    if NewX < 0 or NewX > 4 or NewY > 4 or NewY < 0:
+        print("You can't go that way")
+        NewX = x
+        NewY = y
+        Stuck = True
+
+    return x,y,NewX,NewY,Stuck
+
+
+x=2
+y=2
+
+CurrentCoords = [x,y]
+PastCoords = [x,y]
+def Start():
+    SpawnMonsters()
+    SpawnEvents()
+    SpawnItems()
+    Lore()
+    Rooms[x][y] = "Player"
+
+    PrintGrid()
+
+def PrintGrid():
+    for i in range(len(Rooms)):
+        print(Rooms[i])
+
+
+# def GameLoop():
+#     if Keys == 5:
+#         SpawnBoss()
+
+
+Start()
+
+Directions = ["West", "East", "South", "North"]
+
+CurrentDescription = "You stand in the room where you awoke"
+while True:
+    print(Rooms[x][y])
+    print(Rooms[PastCoords[0]][PastCoords[1]])
+    PrintGrid()
+    print(f"""
+{CurrentDescription}
+
+Which direction do you go?
+1. North
+2. South
+3. East
+4. West""")
+    #print (x,y)
+    array = Move(Directions[int(input(">"))-1],x,y)
+    x = array[2]
+    y = array[3]
+
+    if not array[4]:
+        PastX = array[0]
+        PastY = array[1]
+
+    if Rooms[x][y] == []:
+        CurrentDescription = choice(EmptyRoomDescriptions)
+    else:
+        CurrentDescription = Rooms[x][y]
+
+    PastCoords = [PastX,PastY]
